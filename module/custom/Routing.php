f3654ad3c49f5877438871b7893f33601d31c604
MarkWatney15102 <eikewientjes882@gmail.com>
2019-12-03 19:21:52 +0100

    ADD: Second posible Request to same controller

--- module/custom/Routing.php
+++ module/custom/Routing.php
@@ -18,18 +18,30 @@ class Routing
         $this->getRoutesList();
     }
 
-    public function redirect($requestedUri)
+    public function rout($requestedUri)
     {
         foreach ($this->routes->routes as $route) {
-            if ($requestedUri == $route->request) {
-                require_once($_SERVER['DOCUMENT_ROOT'] . "/" . $route->controller);
-                require_once($_SERVER['DOCUMENT_ROOT'] . "/views/Header/header.php");
-                $controller = new $route->controllerName;
-                $controller->init(new Title(), $this->config);
-                $controller->createView();
+            if ($requestedUri === $route->request) {
+                $this->internalRouting($route);
                 break;
+            } else {
+                if(isset($route->secondRequest)) {
+                    if($requestedUri === $route->secondRequest) {
+                        $this->internalRouting($route);
+                        break;
+                    }
+                }
             }
         }
+    }
+
+    private function internalRouting($route)
+    {
+        require_once($_SERVER['DOCUMENT_ROOT'] . "/" . $route->controller);
+        require_once($_SERVER['DOCUMENT_ROOT'] . "/views/Header/header.php");
+        $controller = new $route->controllerName;
+        $controller->init(new Title(), $this->config);
+        $controller->createView();
     }
 
     private function getRoutesList()
