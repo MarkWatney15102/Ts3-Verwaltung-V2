aa28db4286c64c889ca1404e21423a54131fa915
MarkWatney15102 <42077358+MarkWatney15102@users.noreply.github.com>
2020-01-29 19:15:45 +0100

    ADD: Channel List

---
+++ controller/Channel/ChannelList/ChannelList.php
@@ -0,0 +1,21 @@
+<?php 
+
+class ChannelList implements ControllerInterface
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    public function init(Title $title, Config $config)
+    {
+        $this->config = $config;
+        $title->setTitle("Channel List");
+    }
+
+    public function createView()
+    {
+        require_once($_SERVER['DOCUMENT_ROOT'] . "/views/Channel/ChannelList/ChannelList.php");
+    }
+}
+
